706db527364f19be666107ec7a308e04d0cd7c26
cloudcoke <lms4296@gmail.com>
2024-07-25 18:01:02 +0900

    test3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const https = require("https")
 exports.handler = async (event) => {
     switch (event.functionName) {
         case "Course_history":
-            return Course_history()
+            return await Course_history()
         case "Class_history":
             return Class_history()
         default:
@@ -14,31 +14,46 @@ exports.handler = async (event) => {
     }
 }
 
-const Course_history = () => {
+const fetchData = (url) => {
+    return new Promise((resolve, reject) => {
+        https
+            .get(url, (res) => {
+                let data = ""
+
+                // 데이터를 조각조각 수신합니다.
+                res.on("data", (chunk) => {
+                    data += chunk
+                })
+
+                // 전체 데이터 수신이 완료되면 이 콜백이 호출됩니다.
+                res.on("end", () => {
+                    resolve(data)
+                })
+            })
+            .on("error", (err) => {
+                reject(err)
+            })
+    })
+}
+
+const Course_history = async () => {
     console.log("Course_history")
-    https
-        .get("https://www.google.com", (res) => {
-            let data = ""
-
-            // 데이터를 조각조각 수신합니다.
-            res.on("data", (chunk) => {
-                data += chunk
-            })
-
-            // 전체 데이터 수신이 완료되면 이 콜백이 호출됩니다.
-            res.on("end", () => {
-                console.log(data)
-            })
-        })
-        .on("error", (err) => {
-            console.error("Error: " + err.message)
-        })
-    const response = {
-        statusCode: 200,
-        body: "Function Course_history executed successfully",
+    try {
+        const data = await fetchData("https://www.google.com")
+        console.log(data)
+        const response = {
+            statusCode: 200,
+            body: "Function Course_history executed successfully",
+        }
+        console.log(JSON.stringify(response))
+        return response
+    } catch (err) {
+        console.error("Error: " + err.message)
+        return {
+            statusCode: 500,
+            body: "Internal Server Error",
+        }
     }
-    console.log(JSON.stringify(response))
-    return response
 }
 
 const Class_history = () => {
