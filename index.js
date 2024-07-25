15c2ad7c9c7fe07591552aa0d52e07bcb70575c8
cloudcoke <lms4296@gmail.com>
2024-07-25 14:30:01 +0900

    first

---
+++ index.js
@@ -0,0 +1,31 @@
+exports.handler = async (event) => {
+    switch (event.functionName) {
+        case "Course_history":
+            return Course_history()
+        case "Class_history":
+            return Class_history()
+        default:
+            return {
+                statusCode: 400,
+                body: "Function not found",
+            }
+    }
+}
+
+const Course_history = () => {
+    const response = {
+        statusCode: 200,
+        body: "Function Course_history executed successfully",
+    }
+    console.log(JSON.stringify(response))
+    return response
+}
+
+const Class_history = () => {
+    const response = {
+        statusCode: 200,
+        body: "Function Class_history executed successfully",
+    }
+    console.log(JSON.stringify(response))
+    return response
+}
